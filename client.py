29010116926626f72706d6685eb49ad39c0a0228
jzyzak <joshuazyzak@gmail.com>
2023-07-25 08:53:48 -0400

    commments

--- client.py
+++ client.py
@@ -1,9 +1,11 @@
+# Client class with client attributes/information
 class Client:
     username = ""
     ip_address = ""
     password = ""
     uuid = ""
 
+    # Constructor for client class
     def __init__(self, user, ip, pw, id):
         self.username = user
         self.ip_address = ip
